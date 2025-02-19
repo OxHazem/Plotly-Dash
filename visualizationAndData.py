befd417eebdcbd586681cf1e5f295fc55dd3a1c8
OxHazem <omar_hazem_ahmed@hotmail.com>
2025-02-19 21:48:41 +0200

    folder Structures done

---
+++ visualizationAndData.py
@@ -0,0 +1,15 @@
+
+import plotly as py
+import pandas as pd
+
+#load Data
+df=pd.read_csv("D:\DownLoad\projects\plotly-dash-visualization\data\car_price_dataset.csv")
+data_frame = pd.DataFrame(df)
+#print(data_frame.head()) #checking the first 5 rows of the data
+#checking for na
+#print(df.isna().sum()) #there is no NA 
+
+
+
+
+
