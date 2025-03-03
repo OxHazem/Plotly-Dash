a39cb87117f7a50fddbfd289ae7eb26ba3740430
OxHazem <omar_hazem_ahmed@hotmail.com>
2025-03-03 20:28:01 +0200

    Now the button is now working

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from visualizationAndData import data_profiling
 from dash import Dash
-from pages.Layout1 import layout
+from pages.Layout1 import layout1
 #this will be the main file for the mini project 
 #first we need to make ydata profiling to understand the data
 data_profiling()
@@ -8,5 +8,5 @@ data_profiling()
 #Due to the data profiling we found out that the data is about cars and their prices and it is cleaned and ready to be visualized
 
 app=Dash(__name__)
-app.layout=layout
+app.layout= layout1
 app.run_server(debug=True)
--- pages/Layout1.py
+++ pages/Layout1.py
@@ -2,7 +2,7 @@ from dash import html
 from dash import dcc ,Input,Output,callback
 from visualizationAndData import basic_visualization
 
-layout = html.Div(id='layout1', children=[
+layout1 = html.Div(id='layout1', children=[
     html.Link(rel='stylesheet',href='D:\DownLoad\projects\plotly-dash-visualization\\assets\styles.css'),
     html.Div( id='header' , children=[
     html.H1("Welcome to Car Pricing Dashboard",id="main_header"),
@@ -10,5 +10,22 @@ layout = html.Div(id='layout1', children=[
     html.P("This analysis demonstrates the principal determinant of pricing differentials.",id='overview_paragraph')
     ]),
     html.Div( id='charts' , children=[dcc.Graph(figure=basic_visualization()[0],id='chart1'),dcc.Graph(figure=basic_visualization()[1],id='chart2'),dcc.Graph(figure=basic_visualization()[2],id='chart3' ) 
-    ],),html.Footer(id="footer",children=[html.Button("Next Page",id='submit')])
+    ],),html.Footer(id="footer",children=[html.Button("Next Page",id='submit',n_clicks=0)]),
+    
+    
     ])
+layout2=html.Div(id='layout2' , children=[html.H1("Welcome to Car Pricing Dashboard",id="main_header"),html.H2("Overview of the data",id="overview_header"),html.P("This analysis demonstrates the principal determinant of pricing differentials.",id='overview_paragraph')])
+
+@callback(
+    Output('layout1', 'children'),
+    Input('submit', 'n_clicks')
+)
+def update(n_clicks):
+    if n_clicks ==0:
+        return layout1
+    else:
+        return layout2 
+
+
+
+
