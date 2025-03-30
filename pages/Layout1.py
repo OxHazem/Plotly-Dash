782b22fed48119bdadab2f5914c06e81801f4dbe
OxHazem <omar_hazem_ahmed@hotmail.com>
2025-03-30 15:03:30 +0200

    added the callbacks but still the function

--- pages/Layout1.py
+++ pages/Layout1.py
@@ -17,6 +17,18 @@ layout1 = html.Div(id='layout1', children=[
     ])
 layout2=html.Div(id='layout2' , children=[html.Link(rel='stylesheet',href="D:\DownLoad\projects\plotly-dash-visualization\\assets\style2.css"),html.Button("Previous Page",id='submit2',n_clicks=0),html.H1("This is the second page")])
 
+main_layout=html.Div([
+    dcc.Store(id='page_tracker',data=1),
+    html.Div(id="page_content",children=layout1)
+])
+
+# the next function is part will be the call backs and the function that will change between the layouts 
+@callback(
+  Output('page_content','children'), # wee need to locate where the change will happen
+  Input('submit', 'n_clicks')  
+) # to solve this we will create a new layout (main_layout)
+
+
 
 
 
